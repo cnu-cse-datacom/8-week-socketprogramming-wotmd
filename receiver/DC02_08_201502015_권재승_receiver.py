a3685d256a7988c035a962f45f67c3d6b9e2dceb
wotmd <wotmd8534@naver.com>
2019-05-13 09:42:49 -0700

    modify

--- receiver/DC02_08_201502015_권재승_receiver.py
+++ receiver/DC02_08_201502015_권재승_receiver.py
@@ -1,36 +1,45 @@
 import socket
 
-# md5 file write
 BLOCK = 1024
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('',9000))
 
-# timeout 3s
-server_socket.settimeout(5)
+# timeout 10s
+server_socket.settimeout(10)
 
+# recv server_addr & filename
 data, addr = server_socket.recvfrom(2000)
 print("file recv start from " + addr[0])
 print(data.decode())
 filename = data.decode()
 print("File Name : " + filename)
 
+# recv filesize
 data, addr = server_socket.recvfrom(2000)
 total_size = int(data.decode())
 print("File Size : " + str(total_size))
 
+# empty bytes array
 current_size = 0
 filedata = bytes()
 
+# recv file
 while current_size < total_size:
 	try:
 		data = server_socket.recv(BLOCK)
 	except:
 		break
 	filedata += data
-	current_size += BLOCK
+	current_size += len(data)
+	if(current_size > total_size):
+		current_size = total_size
+	
 	persent = (current_size/total_size)*100
-	print(("current_size / total_size = %d/%d, " % (current_size,total_size)) + str(persent) +"%")
+	
+	msg = ("current_size / total_size = %d/%d, " % (current_size,total_size)) + str(persent) +"%"
+	print(msg)
+	server_socket.sendto(msg.encode(), addr)
 
-
+# write file
 open(filename, "wb").write(filedata)
--- sender/DC02_08_201502015_권재승_sender.py
+++ sender/DC02_08_201502015_권재승_sender.py
@@ -5,12 +5,15 @@ PORT = 9000
 BLOCK = 1024
 
 class ClientSocket():
-	def __init__(self):
+	def __init__(self):	
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.socket.settimeout(0.1)
 	
 	def socket_sendfile(self, filename):
+		# file open and read data
 		filedata = open(filename, "rb").read()
 		total_size = len(filedata)
+		# send total size
 		self.socket.sendto(str(total_size).encode(), (IP, PORT))
 		
 		current_size = 0
@@ -20,15 +23,21 @@ class ClientSocket():
 			self.socket.sendto(senddata, (IP, PORT))
 			current_size+=BLOCK
 			
-			if(current_size > total_size):
-				current_size=total_size
+			try:
+				msg, addr = self.socket.recvfrom(BLOCK)
+			except:
+				pass
 			persent = (current_size/total_size)*100
-			print(("current_size / total_size = %d/%d, " % (current_size,total_size)) + str(persent) +"%")
+			msg = ("current_size / total_size = %d/%d, " % (current_size,total_size)) + str(persent) +"%"
+			print(msg)
 		print("ok")
 		print("file_send_end")
 
 	def main(self):
+		# input file name
 		filename = input("Input your file name : ")
+		
+		# send file name & send file
 		self.socket.sendto(filename.encode(), (IP, PORT))
 		self.socket_sendfile(filename)
 		
